8e213ecb3c0d3f3cbcf0f58e872187970c0d66fc
Werner Robitza <werner.robitza@gmail.com>
2021-03-10 20:59:43 +0100

    add python_requires to setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,9 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
+    python_requires=">=3.5",
     entry_points={
         "console_scripts": [
             "ffmpeg-progress-yield = ffmpeg_progress_yield.__main__:main"
