aef38dc66604051e7b245ccf088af5b030d5d8a2
Werner Robitza <werner.robitza@gmail.com>
2025-06-17 13:41:40 +0200

    changed license specifier

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        "MIT",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
